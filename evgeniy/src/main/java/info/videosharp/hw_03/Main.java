280ce6d18505345589e5f21ec6d71e67a1a23065
Jevgenij Volosatov <fformula@gmail.com>
2017-02-27 08:30:43 +0200

    #6 FindFiles using Resursion and java.io

--- evgeniy/src/main/java/info/videosharp/hw_03/Main.java
+++ evgeniy/src/main/java/info/videosharp/hw_03/Main.java
@@ -2,12 +2,27 @@ package info.videosharp.hw_03;
 
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        testImageDownloader();
+        testFindFiles ();
+    }
+
+    public static void testImageDownloader ()
+    {
         ImageDownloader id = new ImageDownloader();
 
         id.downloadImagesFrom(
                 "http://flangex.herokuapp.com/io/load",
                 "png",
-                "g:\\GIT\\JavaProfessional\\img\\");
+                "");
+    }
+
+    public static void testFindFiles ()
+    {
+        FindFiles ff = new FindFilesRecursionIO();
+        String files [] = ff.findFiles(".", "java");
+        for (String name : files)
+            System.out.println(name);
     }
 }
